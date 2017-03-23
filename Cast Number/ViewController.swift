638d6ad0d48a3f4203e0fa0e619d0055b78ef296
Sultan Magomedkadiev <sultan@magomedkadiev.com>
2017-03-23 23:23:59 +0300

    Initial commit

---
+++ Cast Number/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cast Number
+//
+//  Created by Султан Магомедкадиев on 23/03/2017.
+//  Copyright © 2017 Magomedkadiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
